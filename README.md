c22178fe658c81b706837fd3f5eefdec959ac428
Hannah Yudkin <hannahyudkin@Hannahs-MacBook-Pro-2.local>
2020-02-28 13:04:42 -0500

    Added inquirer/fs.

---
+++ README.md
@@ -0,0 +1,30 @@
+# Title
+
+Description Stuff
+
+## Table of Contents
+
+???
+
+## Installation
+
+None
+
+
+## Usage
+
+For things
+
+
+## License
+
+mit
+
+
+## Contributing
+
+me
+
+## Tests
+
+none
--- index.js
+++ index.js
@@ -1,42 +1,104 @@
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-      },
-      {
-        type: "input",
-        name: "location",
-        message: "Where are you from?"
-      },
-      {
-        type: "input",
-        name: "hobby",
-        message: "What is your favorite hobby?"
-      },
-      {
-        type: "input",
-        name: "food",
-        message: "What is your favorite food?"
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Enter your GitHub Username"
-      },
-      {
-        type: "input",
-        name: "linkedin",
-        message: "Enter your LinkedIn URL."
-      }
+var inquirer = require("inquirer");
+var fs = require("fs");
 
+
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username?"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Where is the title of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage for this project?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What is this project licensed under?"
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Who contributed to this project?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What are the tests associated with this project?"
+  }
 ];
 
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function(err) {
+    if (err) {
+      console.log(err)
+    }
+    console.log("README.md created sucessfully!")
+  })
 }
 
 function init() {
+  inquirer
+    .prompt(questions)
+    .then(answers => {
+      //ajax call -> 
+      // Use user feedback for... whatever!!
+      console.log(answers)
+      const profileString = 
+`# ${answers.title}
 
+${answers.description}
+
+## Table of Contents
+
+???
+
+## Installation
+
+${answers.installation}
+
+
+## Usage
+
+${answers.usage}
+
+
+## License
+
+${answers.license}
+
+
+## Contributing
+
+${answers.contribution}
+
+## Tests
+
+${answers.tests}
+`
+      writeToFile("README.md", profileString)
+    });
 }
+
+
+//https://api.github.com/users/hannahyudkin
+//https://api.github.com/users/hannahyudkin/repos
 
 init();
